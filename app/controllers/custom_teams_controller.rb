155662120d1d01ca63cc7358a261386596f1825f
justintate <justinpjtate1@gmail.com>
2023-03-09 17:20:54 +0000

    Finished CRUD routes for custom teams

--- app/controllers/custom_teams_controller.rb
+++ app/controllers/custom_teams_controller.rb
@@ -12,4 +12,39 @@ class CustomTeamsController < ApplicationController
             CustomTeamPlayer.create(custom_team_id: player[:custom_team_id], player_id: player[:player_id], position: player[:position])
         end
     end
+
+    def show_teams
+        @teams = CustomTeam.joins(:user).select("custom_teams.id", "custom_teams.team_name", "custom_teams.user_id", "users.email").all
+        render json: @teams
+    end
+
+    def show_players
+        @players = CustomTeamPlayer.joins(player: :club).select("custom_team_players.custom_team_id", "custom_team_players.player_id", "players.player_name", "clubs.club_name", "custom_team_players.position", "players.dob").where(custom_team_id: params[:custom_team_id])
+        render json: @players
+    end
+
+    def show_teams_from_user
+        @teams = CustomTeam.joins(:user).select("custom_teams.id", "custom_teams.team_name", "users.email").where(user_id: params[:id])
+        render json: @teams
+    end
+
+    def update_team
+        @team = CustomTeam.find(params[:custom_team_id])
+        @team.update(team_name: params[:custom_team_name])
+        @team.save
+        render json: @team
+    end
+
+    def update_team_players
+        # CustomTeamPlayer.joins(player: :club).select("custom_team_players.custom_team_id", "custom_team_players.player_id", "players.player_name", "clubs.club_name", "custom_team_players.position", "players.dob").where(custom_team_id: params[:custom_team_id]).destroy_all
+        CustomTeamPlayer.where(custom_team_id: params[:players][0][:custom_team_id]).destroy_all
+        params[:players].each do |player|
+            CustomTeamPlayer.create(custom_team_id: player[:custom_team_id], player_id: player[:player_id], position: player[:position])
+        end
+    end
+
+    def delete_team
+        CustomTeamPlayer.where(custom_team_id: params[:custom_team_id]).destroy_all
+        CustomTeam.find(params[:custom_team_id]).destroy
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,12 @@ Rails.application.routes.draw do
   get 'players/club_search'
   post 'custom_teams/new_team'
   post 'custom_teams/save_players'
+  get 'custom_teams/show_teams'
+  post 'custom_teams/show_players'
+  post 'custom_teams/show_teams_from_user'
+  patch 'custom_teams/update_team'
+  post 'custom_teams/update_team_players'
+  delete 'custom_teams/delete_team'
   devise_for :users
   resources :players
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
